446125e54dd18741784375b6bbec6dc4eed9cffd
Kaczy <kaczyf93@gmail.com>
2022-02-07 21:37:44 +0100

    Custom xml quest points reward + custom content notice

--- XmlSpawner Core/XmlQuest/XmlQuestPointsRewards.cs
+++ XmlSpawner Core/XmlQuest/XmlQuestPointsRewards.cs
@@ -17,14 +17,14 @@ namespace Server.Engines.XmlSpawner2
 {
 	public class XmlQuestPointsRewards
 	{
-		public int Cost;       // cost of the reward in credits
+		public int MinPoints;   // the minimum points requirement for the reward
 		public Type  RewardType;   // this will be used to create an instance of the reward
 		public string Name;         // used to describe the reward in the gump
+		public int Cost;       // cost of the reward in credits
 		public int ItemID;     // used for display purposes
 		public int ItemHue;
 		public int yOffset;
 		public object [] RewardArgs; // arguments passed to the reward constructor
-		public int MinPoints;   // the minimum points requirement for the reward
 
 		private static List<XmlQuestPointsRewards> PointsRewardList = new List<XmlQuestPointsRewards>();
 
@@ -32,33 +32,26 @@ namespace Server.Engines.XmlSpawner2
 
 		public XmlQuestPointsRewards(int minpoints, Type reward, string name, int cost, int id, int hue, int yoffset, object[] args)
 		{
+			MinPoints = minpoints;
 			RewardType = reward;
+			Name = name;
 			Cost = cost;
 			ItemID = id;
 			ItemHue = hue;
-			Name = name;
-			RewardArgs = args;
-			MinPoints = minpoints;
 			yOffset = yoffset;
+			RewardArgs = args;
 		}
 
 		public static void Initialize()
 		{
             // these are items as rewards. Note that the args list must match a constructor for the reward type specified.
-            PointsRewardList.Add( new XmlQuestPointsRewards( 1000, typeof(PowerScroll), "105 Smithing powerscroll", 1000, 0x14F0, 0, 5, new object[] { SkillName.Blacksmith, 105 }));
-            PointsRewardList.Add( new XmlQuestPointsRewards( 2000, typeof(PowerScroll), "110 Smithing powerscroll", 2000, 0x14F0, 0x22, 5, new object[] { SkillName.Blacksmith, 110 }));
-            PointsRewardList.Add( new XmlQuestPointsRewards( 4000, typeof(PowerScroll), "115 Smithing powerscroll", 4000, 0x14F0, 0x44, 5, new object[] { SkillName.Blacksmith, 115 }));
-            PointsRewardList.Add( new XmlQuestPointsRewards( 500, typeof(AncientSmithyHammer), "+20 Ancient Smithy Hammer, 50 uses", 500, 0x13E4, 0, 5, new object[] { 20, 50 }));
-            PointsRewardList.Add( new XmlQuestPointsRewards( 200, typeof(ColoredAnvil), "Colored Anvil", 400, 0xFAF, 0, 5, null ));
-            PointsRewardList.Add( new XmlQuestPointsRewards( 100, typeof(PowderOfTemperament), "Powder Of Temperament, 10 uses", 300, 4102, 0, 5, new object[] { 10 }));
-            PointsRewardList.Add( new XmlQuestPointsRewards( 100, typeof(LeatherGlovesOfMining), "+20 Leather Gloves Of Mining", 200, 0x13c6, 0, 5, new object[] { 20 }));
+            PointsRewardList.Add( new XmlQuestPointsRewards( 350, typeof(MinorArtifactDeed), "zwoj z wybieralny artefaktem minor", 350, 0x14F0, 0x495, 5, null ));
+            PointsRewardList.Add( new XmlQuestPointsRewards( 250, typeof(ParoxysmusSwampDragonStatuette), "smok bagienny przedwiecznego", 250, 0x2619, 0x851, 5,null ));
+            PointsRewardList.Add( new XmlQuestPointsRewards( 200, typeof(ScrappersCompendium), "Kompedium Wszelkiej Wiedzy", 200, 0xEFA, 0x494, 5, null ));
+            PointsRewardList.Add( new XmlQuestPointsRewards( 150, typeof(PetBondingDeed), "Zwój Oswajacza", 150, 0x14F0, 0x23C, 5, null ));
 
-			// this is an example of adding a mobile as a reward
-            PointsRewardList.Add( new XmlQuestPointsRewards( 0, typeof(RidableLlama),"Ridable Llama", 1, 0x20f6, 0, -15, null));
-
-            // this is an example of adding an attachment as a reward
-            //PointsRewardList.Add( new XmlQuestPointsRewards( 0, typeof(XmlEnemyMastery), "+200% Balron Mastery for 1 day", 2, 0, 0, 0, new object[] { "Balron", 50, 200, 1440.0 }));
-			//PointsRewardList.Add( new XmlQuestPointsRewards( 0, typeof(XmlStr), "+20 Strength for 1 day", 10, 0, new object[] { 20, 86400.0 }));
+            PointsRewardList.Add( new XmlQuestPointsRewards( 100, typeof(XmlEnemyMastery), "+100% Obrażeń przeciwko aniołom", 100, 0, 0, 5, new object[] { "EtherealWarrior", 50, 100, 1440.0 }));
+            PointsRewardList.Add( new XmlQuestPointsRewards( 50, typeof(XmlStr), "+5 siły na 1 dzień", 50, 0, 0, 5, new object[] { 5, 86400.0 }));
 		}
 	}
 }
